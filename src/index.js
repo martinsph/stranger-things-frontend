420d01a968ced97a828012e78de55217e815b107
martinsph <ph.mmartins@gmail.com>
2022-01-04 12:31:47 -0300

    fix 2 req 8

--- src/index.js
+++ src/index.js
@@ -11,6 +11,5 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
